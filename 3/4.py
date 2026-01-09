4c2011dc41dc696da56c5438690451b48d252473
choiseoyoon0330 <sseo330@naver.com>
2026-01-09 10:48:09 +0900

    Create 4.py

---
+++ 3/4.py
@@ -0,0 +1,11 @@
+n, m = map(int, input().split())
+
+result = 0
+for i in range(n):
+    data = list(map(int, input().split()))
+    min_value = 10001
+    for a in data:
+        min_value = min(a, min_value)
+    result = max(min_value, result)
+
+print(result)
