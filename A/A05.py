143dc05aa5663358af0bc14d070564a30aaf2661
choiseoyoon0330 <sseo330@naver.com>
2026-01-09 22:40:33 +0900

    Create A05.py

---
+++ A/A05.py
@@ -0,0 +1,13 @@
+n, m = map(int, input().split())
+data = list(map(int, input().split()))
+
+array = [0] * 11
+for i in data:
+    array[i] += 1
+
+result = 0
+for i in range(1, m + 1):
+    n -= array[i]
+    result += array[i] * n
+
+print(result)
