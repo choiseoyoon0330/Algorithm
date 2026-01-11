b6c7cef4af8ff0f10ab7bfbc755f0609aed2d072
choiseoyoon0330 <sseo330@naver.com>
2026-01-11 15:43:57 +0900

    Create 2.py

---
+++ 4/2.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+n = input()
+result = 0
+
+for i in range(n + 1):
+    for j in range(60):
+        for k in range(60):
+            if '3' in str(i) + str(j) + str(k):
+                result += 1
+
+print(result)
