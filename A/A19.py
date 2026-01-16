5ccf00641e44d90a2802d0aa4af588e6c0598d43
choiseoyoon0330 <sseo330@naver.com>
2026-01-16 15:11:05 +0900

    Update A19.py

--- A/A19.py
+++ A/A19.py
@@ -10,26 +10,27 @@ def dfs(i, now):
     if i == n:
         min_value = min(min_value, now)
         max_value = max(max_value, now)
+    else:
 
-    if add > 0:
-        add -= 1
-        dfs(i + 1, now + data[i])
-        add += 1
+        if add > 0:
+            add -= 1
+            dfs(i + 1, now + data[i])
+            add += 1
 
-    if sub > 0:
-        sub -= 1
-        dfs(i + 1, now - data[i])
-        sub += 1
+        if sub > 0:
+            sub -= 1
+            dfs(i + 1, now - data[i])
+            sub += 1
 
-    if mul > 0:
-        mul -= 1
-        dfs(i + 1, now * data[i])
-        mul += 1
+        if mul > 0:
+            mul -= 1
+            dfs(i + 1, now * data[i])
+            mul += 1
     
-    if div > 0:
-        div -= 1
-        dfs(i + 1, int(now / data[i]))
-        div += 1
+        if div > 0:
+            div -= 1
+            dfs(i + 1, int(now / data[i]))
+            div += 1
 
 dfs(1, data[0])
 
