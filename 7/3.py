cb24039c20a887425acb29eeeff049b81a4dad33
choiseoyoon0330 <sseo330@naver.com>
2026-01-17 12:23:44 +0900

    Create 3.py

---
+++ 7/3.py
@@ -0,0 +1,19 @@
+def binary_search(array, target, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+        if array[mid] == target:
+            return mid + 1
+        elif array[mid] < target:
+            start = mid + 1
+        else:
+            end = mid - 1
+    return None
+
+n, target = map(int, input().split())
+data = list(map(int, input().split()))
+
+result = binary_search(data, target, 0, n - 1)
+if result == None:
+    print('원소가 존재하지 않습니다.')
+else:
+    print(result)
