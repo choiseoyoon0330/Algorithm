7f850459d4aab2471b0cc5fc6f78a1d45eb4a6b3
choiseoyoon0330 <sseo330@naver.com>
2026-01-23 09:59:22 +0900

    Create A33.py

---
+++ A/A33.py
@@ -0,0 +1,19 @@
+n = int(input())
+dp = [0] * (n + 1)
+t = []
+p = []
+for _ in range(n):
+    time, price = map(int, input().split())
+    t.append(time)
+    p.append(price)
+
+max_value = 0
+for i in range(n - 1, -1, -1):
+    time = i + t[i]
+    if time <= n:
+        dp[i] = max(p[i] + dp[time], max_value)
+        max_value = dp[i]
+    else:
+        dp[i] = max_value
+
+print(max_value)
